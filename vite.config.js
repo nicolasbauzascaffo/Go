9eed03f4f08676b88be307687ced41fefdf96a5c
nicolasbauzaSenpai <nicolasbauza21@gmail.com>
2023-05-04 00:00:41 -0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+      output: {
+        dir: "dist",
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash][extname]",
+      },
+      manualChunks(id) {
+        if (id.includes("node_modules")) {
+          return "vendor";
+        } else {
+          return "app";
+        }
+      },
+      preserveEntrySignatures: "allow-extension",
+      treeshake: true,
+    },
+  },
+});
